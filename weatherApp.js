d944270a4ad04d1ec466e778c26ebef0130af503
rmdaniyal01-max <r.m.daniyal01@gmail.com>
2026-07-18 03:09:53 -0700

    updates

--- weatherApp.js
+++ weatherApp.js
@@ -23,10 +23,30 @@ async function getLocation(city) {
     let location = data.results[0];
 
     let weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location
-        .latitude}&longitude=${location.longitude}&current=temperature_2m,wind_speed_10m,weather_code`);
+        .latitude}&longitude=${location.longitude}&current=temperature_2m,apparent_temperature,wind_speed_10m,weather_code`);
     let weatherData = await weatherResponse.json();
 
     let weatherCode = weatherData.current.weather_code;
+    let weatherIcon = "";
+
+    if(weatherCode === 0){
+        weatherIcon = "☀";
+    }
+    else if(weatherCode >= 1 && weatherCode <= 3){
+        weatherIcon = "⛅";
+    }
+    else if(weatherCode >= 51 && weatherCode <= 67){
+        weatherIcon = "🌧";
+    }
+    else if(weatherCode >= 71 && weatherCode <= 77){
+        weatherIcon = "❄";
+    }
+    else if(weatherCode === 95){
+        weatherIcon = "⛈";
+    }
+    else{
+        weatherIcon = "🌤";
+    }
 
     if(weatherCode === 0){
         document.body.style.background = 
@@ -54,9 +74,20 @@ async function getLocation(city) {
         "linear-gradient(135deg,#616161,#9bc5c3)" 
     }
 
+    let today = new Date();
+
+    let date = today.toLocaleDateString("en-US",{
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    })
+
     weather.innerHTML = `
+    <div class="weather-icon">${weatherIcon}</div>
     <h2>${location.name}</h2>
+    <p class="date">${date}</p>
     <p>Temperature: ${weatherData.current.temperature_2m} °C </p>
+    <p class="feel">Real feel: ${weatherData.current.apparent_temperature} °C </p>
     <p>Wind Speed: ${weatherData.current.wind_speed_10m} Km/h</p>`;
 
 }
