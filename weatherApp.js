ad9c17341acaf2f0bd9bbb2b95b1de2cb5bd30d9
rmdaniyal01-max <r.m.daniyal01@gmail.com>
2026-07-19 17:02:43 +0500

    update

--- weatherApp.js
+++ weatherApp.js
@@ -6,53 +6,12 @@ let weatherCard = document.getElementById("weatherCard");
 
 cityInput.focus();
 
-searchButton.addEventListener("click", () => {
-    let city = cityInput.value.trim();
-    getLocation(city);
-    
-});
+async function getWeather(latitude, longitude){
 
-locationButton.addEventListener("click", () => {
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition((position) => {
-            let latitude = position.coords.latitude;
-            let longitude = position.coords.longitude;
-        })
-    }else{
-        alert("geolocation is not supported");
-    }
-})
-
-cityInput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter"){
-        searchButton.click();
-    };
-});
-
-async function getLocation(city){
-    
-    cityInput.value = "";
-
-    if (city === ""){
-        weather.innerHTML = `<p> Please enter a City</p>`
-        return;
-    }
-    
-        weather.innerHTML = `<p>Loading....</p>`
-
-    let response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`);
-    let data = await response.json();
-    
-    if(!data.results || data.results.length === 0){
-        weather.innerHTML = `<p>City not found</p>`
-        return;
-    }
-    let location = data.results[0];
-
-    let weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${location
-        .latitude}&longitude=${location.longitude}&current=temperature_2m,apparent_temperature,wind_speed_10m,weather_code,relative_humidity_2m&daily=sunrise,sunset&timezone=auto`);
+    let weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${
+        latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,wind_speed_10m,weather_code,relative_humidity_2m&daily=sunrise,sunset&timezone=auto`);
     let weatherData = await weatherResponse.json();
-    console.log(weatherData)
+    
 
     let weatherCode = weatherData.current.weather_code;
     let weatherIcon = "";
@@ -85,7 +44,7 @@ async function getLocation(city){
         "linear-gradient(135deg,#a1c4fd,#c2e9fb)"
     }
     else if(weatherCode >=51){
-       document.body.style.background = 
+    document.body.style.background = 
         "linear-gradient(135deg,#616161,#9bc5c3)" 
     }
 
@@ -98,7 +57,7 @@ async function getLocation(city){
         "linear-gradient(135deg,#a1c4fd,#c2e9fb)"
     }
     else if(weatherCode >=51){
-       weatherCard.style.backgroundColor = 
+    weatherCard.style.backgroundColor = 
         "linear-gradient(135deg,#616161,#9bc5c3)" 
     }
 
@@ -134,7 +93,7 @@ async function getLocation(city){
     else if(hour<18){
         greeting="☀️ Good Afternoon";
     }
-    else if(hour >18 && hour <22){
+    else if(hour<22){
         greeting="🌙 Good Evening";
     }
     else{
@@ -183,4 +142,54 @@ async function getLocation(city){
             <p>${sunset}</p>
         </div>
     </div>`;
+
 }
+
+async function getLocation(city){
+    
+    cityInput.value = "";
+
+    if (city === ""){
+        weather.innerHTML = `<p> Please enter a City</p>`
+        return;
+    }
+    
+        weather.innerHTML = `<p>Loading....</p>`
+
+    let response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`);
+    let data = await response.json();
+    
+    if(!data.results || data.results.length === 0){
+        weather.innerHTML = `<p>City not found</p>`
+        return;
+    }
+    let location = data.results[0];
+
+    getWeather(location.latitude, location.longitude, location);
+
+}
+
+
+searchButton.addEventListener("click", () => {
+    let city = cityInput.value.trim();
+    getLocation(city);
+    
+});
+
+
+cityInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter"){
+        searchButton.click();
+    };
+});
+
+
+
+locationButton.addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+        getWeather(
+            position.coords.latitude,
+            position.coords.longitude
+        )
+    })
+})
